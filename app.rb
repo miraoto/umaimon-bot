7f468cf19dfe219b9976426701694f876a3c3e55
miraoto <miraoto@gmail.com>
2016-04-21 13:14:02 +0900

    語尾にらっきょをつける

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
         to: [msg['content']['from']],
         toChannel: 1383378250, # Fixed  value
         eventType: "138311608800106203", # Fixed value
-        content: msg['content']
+        content: "#{msg['content']}だらっきょ"
       }
 
       endpoint_uri = 'https://trialbot-api.line.me/v1/events'
