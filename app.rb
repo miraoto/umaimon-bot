65b1169e13b9e1d6b89d8f4c742b4104c3574662
miraoto <miraoto@gmail.com>
2016-05-17 10:37:30 +0900

    debug

--- app.rb
+++ app.rb
@@ -47,7 +47,8 @@ class Translate
       chiebukuro_api_url = 'http://chiebukuro.yahooapis.jp/Chiebukuro/V1/questionSearch'
       response = RestClient.get(chiebukuro_api_url, { params: { appid: ENV['YAHOO_APP_ID'], query: text, results: 1, condition: 'solved' } })
       doc = REXML::Document.new(response)
-      response = doc.elements['ResultSet/Result/Question/BestAnswer'].text
+      response = doc.elements['ResultSet/Result/Question/BestAnswer'].text.truncate(30) +
+                 doc.elements['ResultSet/Result/Question/Url'].text
     rescue => e
       response = 'うまく認識できなかったよー'
       p e
