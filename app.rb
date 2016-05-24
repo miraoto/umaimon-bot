0716c3847df3040101143417df1a0548b60a8b28
miraoto <miraoto@gmail.com>
2016-05-24 10:29:06 +0900

    change facebook recomment function

--- app.rb
+++ app.rb
@@ -32,13 +32,28 @@ class App < Sinatra::Base
     "OK"
   end
 
-  get '/facebookbot/callback' do
-     if params['hub.verify_token'] == ENV['FACEBOOK_VERIFY_TOKEN']
-       p params
-       params['hub.challenge']
-     else
-       'invalid verify token'
-     end
+  post '/facebookbot/callback' do
+    token = ENV["FACEBOOK_TOKEN"]
+
+    message = params["entry"][0]["messaging"][0]    
+
+    if message.include?("message")
+      sender = message["sender"]["id"]
+      text = message["message"]["text"]
+      endpoint_uri = "https://graph.facebook.com/v2.6/me/messages?access_token=" + token
+      request_content = {recipient: {id:sender},
+                         message: {text: text}
+                        }
+      content_json = request_content.to_json
+
+      RestClient.post(endpoint_uri, content_json, {
+        'Content-Type' => 'application/json; charset=UTF-8'
+      }){ |response, request, result, &block|
+        p response
+        p request
+        p result
+      }
+    end
   end
 end
 
