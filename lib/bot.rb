d81fe60320a98ad4a260cd2d2c0bd3863cdc4c4d
miraoto <miraoto@gmail.com>
2016-05-25 09:37:44 +0900

    refactor

--- lib/bot.rb
+++ lib/bot.rb
@@ -4,21 +4,27 @@ class Bot
       faq(text) 
     else
       talk(text)
-      "#{text}っていいました？"
     end
   end
 
   def self.faq(text)
     begin
+      request_content = {
+        params: {
+          appid: ENV['YAHOO_APP_ID'],
+          query: text,
+          results: 1,
+          condition: 'solved'
+        }
+      }
       chiebukuro_api_url = 'http://chiebukuro.yahooapis.jp/Chiebukuro/V1/questionSearch'
-      response = RestClient.get(chiebukuro_api_url, { params: { appid: ENV['YAHOO_APP_ID'], query: text, results: 1, condition: 'solved' } })
+      response = RestClient.get(chiebukuro_api_url, request_content)
       doc = REXML::Document.new(response)
-      response = "#{doc.elements['ResultSet/Result/Question/BestAnswer'].text.slice(0, 30)}...#{doc.elements['ResultSet/Result/Question/Url'].text}"
+      reply = "#{doc.elements['ResultSet/Result/Question/BestAnswer'].text.slice(0, 30)}...#{doc.elements['ResultSet/Result/Question/Url'].text}"
     rescue => e
-      response = 'うまく認識できなかったよー'
-      p e
+      reply = '質問内容をもう一度教えてもらえますか？'
     end
-    response
+    reply
   end
 
   def self.talk(text)
@@ -27,14 +33,12 @@ class Bot
         utt: text
       }.to_json
       talk_api_url = "https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=#{ENV['DOCOMO_APP_KEY']}"
-      response = RestClient.post(talk_api_url, request_content, {
+      reply = RestClient.post(talk_api_url, request_content, {
         'Content-Type' => 'application/json; charset=UTF-8',
-      })
-      p response
+      })['utt']
     rescue => e
-      response = 'うまく認識できなかったよー'
-      p "error:#{e}"
+      reply = "#{text}っていいました？"
     end
-    response
+    reply
   end
 end
